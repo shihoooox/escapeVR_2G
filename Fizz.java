b677930bf4d8d13cf44e0e863020057015029bdd
宮崎 光 <satosatochibi@gmail.com>
2016-07-08 20:08:24 +0900

    “Fizzの機能を作成した”

---
+++ Fizz.java
@@ -0,0 +1,13 @@
+public class Fizz extends Object{
+	Fizz(){
+		super();	
+	}
+
+	public static void main(String[] args){
+		print();
+	}
+
+	public static void print(){
+		System.out.printf("Fizzが呼ばれました\n");
+	}
+}
